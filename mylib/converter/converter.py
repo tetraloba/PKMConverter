296c5e4db2dd8ad253a5818559ab94acf5a80cb0
tetraloba <tetraloba@gmail.com>
2026-09-12 20:50:17 +0900

    SBPage2NDPage ignore tags in code blocks (German2)

--- mylib/converter/converter.py
+++ mylib/converter/converter.py
@@ -21,10 +21,41 @@ def GKMemo2SBPage(memo: GKMemo):
         lines = [memo.title] + memo.textContent.split('\n') + [importation_tag, tags, state_tags]
     )
 
+def _get_codeblock_linenums_from_SBPage(sbPage: SBPage):
+    """
+    SBPageのlinesの内，コードブロック(code:)である行範囲(半開区間)のリストを返す。
+    10~13行目と22~47行目がコードブロックなら戻り値は[(10,14),(22,48)]
+    """
+    cbs = []
+    begin = -1
+    for i, line in enumerate(sbPage.lines):
+        if begin != -1:
+            if len(line) and (line[0] == ' ' or line[0] == '\t'):
+                continue
+            else:
+                end = i + 1
+                cbs.append((begin, end))
+                begin = -1
+        if line[0:5] == 'code:':
+            begin = i
+    if begin != -1:
+        end = i + 1
+        cbs.append((begin, end))
+    return cbs
+
 def _get_tags_from_SBPage(sbPage: SBPage):
     tags = []
     # except code block #TODO
-    for line in sbPage.lines:
+    cbs = _get_codeblock_linenums_from_SBPage(sbPage)
+    cbs_i = 0
+    for i, line in enumerate(sbPage.lines):
+        if cbs_i < len(cbs) and cbs[cbs_i][1] <= i:
+            cbs_i += 1
+        if cbs_i < len(cbs) and cbs[cbs_i][1] <= i:
+            raise RuntimeError("codeblock range with same 'end' detected!")
+        if cbs_i < len(cbs) and cbs[cbs_i][0] <= i:
+            # is codeblock
+            continue
         TAG = r'(?:^|\s)(#[^\s]+)' # tag pettern
         tags += re.findall(TAG, line)
 
