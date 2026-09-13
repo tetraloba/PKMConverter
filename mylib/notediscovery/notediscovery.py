06ea96d0421d1e8fcddea7945a448125288911ed
tetraloba <tetraloba@gmail.com>
2026-09-13 20:34:24 +0900

    notediscovery add generate_filename() (German2)

--- mylib/notediscovery/notediscovery.py
+++ mylib/notediscovery/notediscovery.py
@@ -27,20 +27,20 @@ yaml.add_representer(
 )
 
 class Page:
+    def generate_filename(title: str, created: int):
+        created_dt = datetime.fromtimestamp(created)
+        created_dt_str = f"{created_dt.year:04}{created_dt.month:02}{created_dt.day:02}-{created_dt.hour:02}{created_dt.minute:02}{created_dt.second:02}"
+        unsafe_filename = f"{created_dt_str}_{title}"
+        INVALID_CHARS = r'[\\/:*?"<>|]' # characters can't be used as filename
+        filename = re.sub(INVALID_CHARS, '_', unsafe_filename)
+        return filename
     def __init__(self, title: str, created: int, updated: int, tags: list[str], content: str):
         self._title: Title = Title(title)
         self._created: int = created # [seconds]
         self._updated: int = updated # [seconds]
         self._tags: list[str] = tags # tag format is 'tagname', no '#'
         self._content: str = content
-        self._set_filename(self._title, self._created)
-    def _set_filename(self, title: Title, created: int):
-        created_dt = datetime.fromtimestamp(created)
-        created_dt_str = f"{created_dt.year:04}{created_dt.month:02}{created_dt.day:02}-{created_dt.hour:02}{created_dt.minute:02}{created_dt.second:02}"
-        unsafe_filename = f"{created_dt_str}_{title}"
-        INVALID_CHARS = r'[\\/:*?"<>|]' # characters can't be used as filename
-        filename = re.sub(INVALID_CHARS, '_', unsafe_filename)
-        self._filename: str = filename
+        self._filename = Page.generate_filename(self._title, self._created)
     def from_file(filepath):
         with open(filepath, 'r') as f:
             #TODO
